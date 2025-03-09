4d5b3874222505cf42adcb0fa523af57905defcf
Aalok <dubeyaalok12@gmail.com>
2025-03-09 21:34:27 +0530

    formik

--- src/pages/LeaveReq.jsx
+++ src/pages/LeaveReq.jsx
@@ -8,11 +8,12 @@ const LeaveReq = () => {
         designation:Yup.string().required("Designation is Required!"),
         department:Yup.string().required("Department is Required!"),
         site:Yup.string().required("Sitecode is Required!"),
-        email: Yup.string().email("Invalid email").required("Email is required"),
-        password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
-        confirmPassword: Yup.string()
-          .oneOf([Yup.ref("password"), null], "Passwords must match")
-          .required("Confirm Password is required"),
+        reasonforleave:Yup.string().required("Reason For Leave is Required!"),
+        startdate:Yup.string().required("Start Date is Required!"),
+        enddate:Yup.string().required("End Date is Required!"),
+        ttldays:Yup.string().required("Total Days is Required!"),
+        reasondoc:Yup.string().required("Ducument is Required!"),
+        submitto:Yup.string().required("Submit To is Required!"),
       });
   return (
     <div className="container">
@@ -29,9 +30,12 @@ const LeaveReq = () => {
                 site:"1",
                 plcr:10,
                 mlcr:5,
-                email: "",
-                password: "",
-                confirmPassword: "",
+                reasonforleave:"",
+                startdate:"",
+                enddate:"",
+                ttldays:0,
+                reasondoc:"",
+                submitto:"-1",
               }}
               validationSchema={validationSchema}
               onSubmit={(values, { resetForm }) => {
@@ -90,34 +94,54 @@ const LeaveReq = () => {
                             <label className="form-label">Nature of Leave</label>
                             <Field as="select" name="natureofleave" className="form-control">
                                 <option value="-1">--Select--</option> 
-                                <option value="1">MUMB</option>
-                                <option value="2">BIAL</option>
-                                <option value="3">GIAL</option>
+                                <option value="1">Privilege Leave</option>
+                                <option value="2">Medical Leave</option>
                             </Field>
                             <ErrorMessage name="natureofleave" component="div" className="text-danger" />
                         </div>
                     </div>
-
-                  <div className="mb-3">
-                    <label className="form-label">Email</label>
-                    <Field type="email" name="email" className="form-control" />
-                    <ErrorMessage name="email" component="div" className="text-danger" />
-                  </div>
-
-                  <div className="mb-3">
-                    <label className="form-label">Password</label>
-                    <Field type="password" name="password" className="form-control" />
-                    <ErrorMessage name="password" component="div" className="text-danger" />
-                  </div>
-
-                  <div className="mb-3">
-                    <label className="form-label">Confirm Password</label>
-                    <Field type="password" name="confirmPassword" className="form-control" />
-                    <ErrorMessage name="confirmPassword" component="div" className="text-danger" />
-                  </div>
+                    <div className='row'>
+                        <div className="col-sm-3 mb-3">
+                            <label className="form-label">Reason For Leave</label>
+                            <Field type="text" name="reasonforleave" className="form-control" readOnly />
+                            <ErrorMessage name="reasonforleave" component="div" className="text-danger" />
+                        </div>
+                        <div className="col-sm-3 mb-3">
+                            <label className="form-label">Start Date</label>
+                            <Field type="date" name="startdate" className="form-control" />
+                            <ErrorMessage name="startdate" component="div" className="text-danger" />
+                        </div>
+                        <div className="col-sm-3 mb-3">
+                            <label className="form-label">End Date</label>
+                            <Field type="date" name="enddate" className="form-control" />
+                            <ErrorMessage name="enddate" component="div" className="text-danger" />
+                        </div>
+                        <div className="col-sm-3 mb-3">
+                            <label className="form-label">Total Days</label>
+                            <Field type="number" name="ttldays" className="form-control" readOnly />
+                            <ErrorMessage name="ttldays" component="div" className="text-danger" />
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className="col-sm-3 mb-3">
+                            <label className="form-label">Document</label>
+                            <Field type="file" name="reasondoc" className="form-control" readOnly />
+                            <ErrorMessage name="reasondoc" component="div" className="text-danger" />
+                        </div>
+                        <div className="col-sm-3 mb-3">
+                            <label className="form-label">Submit To</label>
+                            <Field as="select" name="submitto" className="form-control">
+                                <option value="-1">--Select--</option> 
+                                <option value="1">N Santosh Kumar</option>
+                                <option value="2">DInesh Dokania</option>
+                            </Field>
+                            <ErrorMessage name="submitto" component="div" className="text-danger" />
+                        </div>
+                        
+                    </div>
 
                   <button type="submit" className="btn btn-primary w-100" disabled={isSubmitting}>
-                    Register
+                    Submit Request
                   </button>
                 </Form>
               )}
